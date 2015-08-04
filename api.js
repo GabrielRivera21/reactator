0bfb5a70a63d6ffabb87d45e781d366d6e473836
Armin Bahramshahry <arminbhy@gmail.com>
2015-08-03 20:36:56 -0700

    First attempt at a doc page

---
+++ api.js
@@ -0,0 +1,31 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "AjaxClient",
+        "AppAction",
+        "AppConstants",
+        "AppDispatcher",
+        "AppStarter",
+        "AppStore",
+        "BasicLayout",
+        "BootstrapModal",
+        "BootstrapModalMixin",
+        "Client",
+        "ClientError",
+        "ClientResponse",
+        "ClientWrapper",
+        "CompositeRoutes",
+        "DispatcherFactory",
+        "JestPreprocessor",
+        "MetaDataComponent",
+        "Q",
+        "ResponsiveComponent",
+        "Routes",
+        "Store",
+        "U",
+        "jQuery"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
