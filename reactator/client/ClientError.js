a8d196d194fabb1a70fe11446d417921c5dff33a
Armin Bahramshahry <arminbhy@gmail.com>
2016-09-06 21:27:53 -0700

    error to wrap other errors

--- reactator/client/ClientError.js
+++ reactator/client/ClientError.js
@@ -4,15 +4,20 @@ import _ from '../lib/lodash.js';
  * @param {Number} status http status code of the error
  * @param {String} message message of the error
  * @param {String} request request that failed
+ * @param {Error} error error to wrap
  * @class
  * @classdesc Error thrown by {@link module:Reactator.Client} on failed operations.
  * @memberof module:Reactator
  */
-function ClientError(status, message, request) {
+function ClientError(status, message, request, error) {
     this.name = 'ClientError';
     this.status = status || 400;
     this.message = message || 'No message.';
     this.request = request || 'Request not specified.';
+
+    if (error) {
+        this.stack = error.stack;
+    }
 }
 
 ClientError.prototype = Object.create(Error.prototype);
@@ -55,5 +60,20 @@ _.extend(ClientError.prototype, {
         return this.status === 503;
     }
 });
+
+/**
+ * @param {Error} error error to wrap
+ * @param {Status} status status to set if needed
+ * @param {String} message message to set if needed
+ * @param {String} request request that failed
+ * @return {ClientError} Error of type ClientError
+ */
+ClientError.from = function(error, status, message, request) {
+    if (error instanceof ClientError) {
+        return error;
+    }
+
+    return new ClientError(status, message || error.message, request, error);
+};
 
 module.exports = ClientError;
